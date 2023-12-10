d1437a235a05c0876851833182f7ebb69ae3085a
Hnat <hnat.ilkiv.ir.2022@lpnu.ua>
2023-12-10 23:06:00 +0200

    right_rectangle_method & left_rectangle_method & trapezoidal_method

---
+++ main.py
@@ -0,0 +1,51 @@
+import math
+
+# Функція, яку інтегруємо
+def f(x):
+    return x * math.sqrt((x**2 - 4)**3)
+
+# Метод правих прямокутників
+def right_rectangle_method(a, b, n):
+    h = (b - a) / n
+    result = 0
+    for i in range(1, n + 1):
+        result += f(a + i * h)
+    result *= h
+    return result
+
+# Метод лівих прямокутників
+def left_rectangle_method(a, b, n):
+    h = (b - a) / n
+    result = 0
+    for i in range(n):
+        result += f(a + i * h)
+    result *= h
+    return result
+
+# Метод трапецій
+def trapezoidal_method(a, b, n):
+    h = (b - a) / n
+    result = 0.5 * (f(a) + f(b))
+    for i in range(1, n):
+        result += f(a + i * h)
+    result *= h
+    return result
+
+# Значення інтегралу за допомогою методів
+a = 2
+b = 3
+n = 30
+
+true_result = math.sqrt(125)
+
+integral_right_rectangle = right_rectangle_method(a, b, n)
+integral_left_rectangle = left_rectangle_method(a, b, n)
+integral_trapezoidal = trapezoidal_method(a, b, n)
+
+# Result ~11.18034
+print(f"|           Метод               |  Результат  |")
+print(f"|За методом правих прямокутників|{integral_right_rectangle:.10f}|")
+print(f"|За методом лівих прямокутників |{integral_left_rectangle:.10f}|")
+print(f"|За методом трапецій            |{integral_trapezoidal:.10f}|")
+print(f"|Очікуваний результат           |{true_result:.10f}|")
+
