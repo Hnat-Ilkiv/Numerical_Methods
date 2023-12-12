9ff01ad47b5fc6111362d141a03ba53d51d1f8aa
Hnat <hnat.ilkiv.ir.2022@lpnu.ua>
2023-12-12 11:57:00 +0200

    final commit

--- main.py
+++ main.py
@@ -5,46 +5,60 @@ def multiply_matrix_vector(matrix, vector):
     return [sum(a * b for a, b in zip(row, vector)) for row in matrix]
 
 
+
+def F(x, y):
+    return x**2 - y**2 + 0.1 - x  # Перше рівняння
+
+def G(x, y):
+    return 2*x*y - 0.1 - y  # Друге рівняння
+
+def partial_derivative_x(func, x, y, epsilon=1e-6):
+    return (func(x + epsilon, y) - func(x, y)) / epsilon
+
+def partial_derivative_y(func, x, y, epsilon=1e-6):
+    return (func(x, y + epsilon) - func(x, y)) / epsilon
+
+def jacobian_matrix(x, y, epsilon=1e-6):
+    dfdx = partial_derivative_x(F, x, y, epsilon)
+    dfdy = partial_derivative_y(F, x, y, epsilon)
+    dgdx = partial_derivative_x(G, x, y, epsilon)
+    dgdy = partial_derivative_y(G, x, y, epsilon)
+    return [[dfdx, dfdy], [dgdx, dgdy]]
+
 #
 # Метод Ньютона з кінцево-різницевою матрицею Якобі
 #
+def newton_method(x0, y0, max_iterations=100, epsilon=1e-6):
+    for iteration in range(max_iterations):
+        f_x = F(x0, y0)
+        g_x = G(x0, y0)
 
-def newton_method(x0, epsilon, max_iter=100):
-    x = x0[:]
-    identity_matrix = [[1 if i == j else 0 for j in range(len(x))] for i in range(len(x))]
+        jacobian = jacobian_matrix(x0, y0, epsilon)
+        determinant = jacobian[0][0] * jacobian[1][1] - jacobian[0][1] * jacobian[1][0]
 
-    for _ in range(max_iter):
-        # Обчислення значень функцій та матриці Якобі
-        f = [
-            x[0]**2 - x[1]**2 + 0.1 - x[0],
-            2*x[0]*x[1] - 0.1 - x[1]
+        jacobian_inv = [
+            [jacobian[1][1] / determinant, -jacobian[0][1] / determinant],
+            [-jacobian[1][0] / determinant, jacobian[0][0] / determinant]
         ]
-        
-        J = [
-            [2*x[0] - 1, -2*x[1]],
-            [2*x[1], 2*x[0] - 1]
-        ]
-        
-        # Обертання матриці Якобі
-        J_inv = [[J[1][1], -J[0][1]], [-J[1][0], J[0][0]]]
-        determinant = J[0][0] * J[1][1] - J[0][1] * J[1][0]
-        J_inv = [[elem / determinant for elem in row] for row in J_inv]
-        
-        # Ітераційна формула
-        delta_x = multiply_matrix_vector(J_inv, f)
-        x = subtract_vectors(x, delta_x)
-        
-        # Перевірка умови збіжності
-        if max(abs(elem) for elem in f) < epsilon:
+
+        delta_x, delta_y = (
+            jacobian_inv[0][0] * f_x + jacobian_inv[0][1] * g_x,
+            jacobian_inv[1][0] * f_x + jacobian_inv[1][1] * g_x
+        )
+
+        x0 -= delta_x
+        y0 -= delta_y
+
+        if abs(f_x) < epsilon and abs(g_x) < epsilon:
             break
-    
-    return x
+
+    return x0, y0
 
 # Виклик функції для заданого початкового наближення та відносної похибки
 x0 = [1, 1]
 epsilon = 5e-10
-result = newton_method(x0, epsilon)
-print("Результат методу Ньютона:", result)
+result = newton_method(x0[0], x0[1], 100, epsilon)
+print("Метод Ньютона з кінцево-різницевою матрицею Якобі:", result)
 
 #
 # 𝜀-алгоритм
