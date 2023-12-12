bf7a2db71b86378b33db33ca05e5c95acdf72cda
Hnat <hnat.ilkiv.ir.2022@lpnu.ua>
2023-12-12 10:39:51 +0200

    final commit

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import math
 def f(x):
     return x * math.sqrt((x**2 - 4)**3)
 
+def F(x):
+    return math.sqrt((x**2 - 4)**5) / 5
+
 # Метод правих прямокутників
 def right_rectangle_method(a, b, n):
     h = (b - a) / n
@@ -36,16 +39,15 @@ a = 2
 b = 3
 n = 30
 
-true_result = math.sqrt(125)
-
+true_result = F(b) - F(a)
 integral_right_rectangle = right_rectangle_method(a, b, n)
 integral_left_rectangle = left_rectangle_method(a, b, n)
 integral_trapezoidal = trapezoidal_method(a, b, n)
 
 # Result ~11.18034
-print(f"|           Метод               |  Результат  |")
-print(f"|За методом правих прямокутників|{integral_right_rectangle:.10f}|")
-print(f"|За методом лівих прямокутників |{integral_left_rectangle:.10f}|")
-print(f"|За методом трапецій            |{integral_trapezoidal:.10f}|")
-print(f"|Очікуваний результат           |{true_result:.10f}|")
+print(f"|           Метод               |  Результат  |Відносна похибка|")
+print(f"|За методом правих прямокутників|{integral_right_rectangle:.10f}|   {(true_result - integral_right_rectangle):.10f}|")
+print(f"|За методом лівих прямокутників |{integral_left_rectangle:.10f}|    {(true_result - integral_left_rectangle):.10f}|")
+print(f"|За методом трапецій            |{integral_trapezoidal:.10f}|   {(true_result - integral_trapezoidal):.10f}|")
+print(f"|Очікуваний результат           |{true_result:.10f}|    {(true_result - true_result):.10f}|")
 
